f22c8b867aea86419a74d87748b0be197497fa1b
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-13 15:40:24 -0500

    Variables con let y const

---
+++ js/02.js
@@ -0,0 +1,20 @@
+//Variables
+/*Ya no se utiliza*/
+let producto = 'Audifonos Gamer'; //Iniciar variable y asignar valor
+
+let disponible; //Asignación de valor a la variable a futuro
+
+// producto = true; //Reasignando el valor de la variable.
+disponible = true;
+
+let producto1 = 'computadora', //asignación de multiples variables
+    disponible1 = true,
+    categoria = 'computadoras';
+
+    //Estilos de varibales
+let nombre_producto = 'Monitor HD'; //undercase
+let nombreProducto = 'Monitor HD'; //CamelCase
+let NombreProducto = 'Monitor HD'; //Pascal Case
+let nombreproducto = 'Monitor HD';//sin nombre
+
+    console.log(producto)
---
+++ js/03.js
@@ -0,0 +1,20 @@
+//Variables
+/*Ya no se utiliza*/
+const producto = 'Audifonos Gamer' //Iniciar variable y asignar valor
+
+const disponible; //Asignación de valor a la variable a futuro
+
+// producto = true; //Reasignando el valor de la variable.
+disponible = true;
+
+const producto1 = 'computadora', //asignación de multiples variables
+    disponible1 = true;
+    categoria = 'computadoras';
+
+    //Estilos de varibales
+const nombre_producto = 'Monitor HD'; //undercase
+const nombreProducto = 'Monitor HD'; //CamelCase
+const NombreProducto = 'Monitor HD'; //Pascal Case
+const nombreproducto = 'Monitor HD';//sin nombre
+
+    console.log(producto)
