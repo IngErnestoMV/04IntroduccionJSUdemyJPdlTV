a1beb76748f5d84d85d57de10bc2c3509fd94808
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-13 18:42:56 -0500

    Strings o Cadenas de Texto

---
+++ js/04.js
@@ -0,0 +1,9 @@
+//String o Cadenas de Texto
+const producto = "Monitor de 20 Pulgadas";
+const producto2 = String('Monitor 30 Pulgadas');
+const producto3 = new String('Monitor 50 Pulgadas');
+
+
+console.log(typeof producto);
+console.log(typeof producto2);
+console.log(typeof producto3);
