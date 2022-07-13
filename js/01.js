4b24b6cb884117229e046b76e3063bc6303597ac
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-13 15:06:05 -0500

    Variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,20 @@
+//Variables
+/*Ya no se utiliza*/
+var producto = 'Audifonos Gamer' //Iniciar variable y asignar valor
+
+var disponible; //Asignación de valor a la variable a futuro
+
+// producto = true; //Reasignando el valor de la variable.
+disponible = true;
+
+var producto1 = 'computadora', //asignación de multiples variables
+    disponible = true;
+    categoria = 'computadoras';
+
+    //Estilos de varibales
+var nombre_producto = 'Monitor HD'; //undercase
+var nombreProducto = 'Monitor HD'; //CamelCase
+var NombreProducto = 'Monitor HD'; //Pascal Case
+var nombreproducto = 'Monitor HD';//sin nombre
+
+    console.log(producto)
