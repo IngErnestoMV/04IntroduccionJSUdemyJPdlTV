e58b22810d7f2f68a234dd8481b4e43814da3649
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-14 00:36:25 -0500

    Controlar el Orden de las Operaciones

---
+++ js/07.js
@@ -0,0 +1,23 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado=20 + 30 * 2; //primero la multiplicación despues la suma se pueden usar parentesis para dar prioridad a la suma.
+
+resultado = (600 + 600) * 1.05;// Ejemplo cobro de comisión sobre el producto.
+
+
+// console.log(resultado);
+
+//Incrementos
+
+let  puntaje = 10;
+
+// Incremento en 1
+puntaje++;//10
+++puntaje;//12
+puntaje--;//12
+--puntaje;//10
+puntaje+= 10;//20
+
+console.log(puntaje);
