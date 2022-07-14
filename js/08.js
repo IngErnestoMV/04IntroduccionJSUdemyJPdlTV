86c75210ba3184fd933562e8fcfe2ef8d7657574
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-14 01:38:44 -0500

    Concactenación y Template Strings

---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Ernesto';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template Strings - string literals || Comillas invertidas: alt + 96
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
