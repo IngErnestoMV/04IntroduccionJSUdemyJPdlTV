d042d83aa8718e904057d4fc17a1014b6bde9b2a
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-23 02:49:41 -0500

    POO - Herencia

--- js/28.js
+++ js/28.js
@@ -12,5 +12,18 @@ class Producto {
 const producto2 = new Producto('Monitor Curvo de 49"', 800);
 const producto3 = new Producto('Laptop', 300);
 
-console.log(producto2);
-console.log(producto3);
+// Herencia
+class Libro extends Producto{ //extends para heredar de otro objeto
+    constructor(nombre, precio, isbn){
+        super(nombre, precio); // super para indicar que caracteristicas se heredan
+        this.isbn = isbn;
+    }
+
+    formatearProducto(){
+        return `${super.formatearProducto()} y su ISBN es: ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JavaSacript La revolución', 120, '910313981389139');
+console.log(libro.formatearProducto());
+console.log(producto2.formatearProducto());
