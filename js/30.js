7585c37b3dc73af7adb62b31cfbdf3950d7e1c73
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-27 21:05:16 -0500

    Promises en JavaScript

---
+++ js/30.js
@@ -0,0 +1,21 @@
+const usuarioAutenticado = new Promise( (resolve, reject) => {
+    const auth = true;
+
+    if(auth) {
+        resolve('Usuario Autenticado'); // El promis se cumple
+    }else{
+        reject('No se pudo iniciar sesión'); // El promise no se cumple
+    }
+});
+
+usuarioAutenticado
+    .then(resultado => console.log(resultado))
+    .catch(error => console.log(error))
+
+console.log(usuarioAutenticado);
+
+// En los Promises existen 3 valores
+// Pending: No se ha cumplido pero tampoco se ha rechazado
+// Fulfilled: Ya se cumplio
+// Rejected: Se ha rechazadoo o no se pudo cumplir.
+
